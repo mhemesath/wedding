1835d1889a1b4e0f4c8298d21e61275729f0c518
Mike Hemesath <mike.hemesath@gmail.com>
2013-07-20 09:29:49 -0500

    Hooked up RSVP

--- app.js
+++ app.js
@@ -5,27 +5,76 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , mongoose = require('mongoose');
 
-var app = express();
+var app = express()
+  , Schema = mongoose.Schema
+  , ObjectId = Schema.ObjectId;
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(express.compress());
   app.use(express.favicon());
   app.use(express.logger('dev'));
+  app.use(express.cookieParser('red velvet'));
+  app.use(express.cookieSession());
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
+  app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.send(500, 'Something broke!');
+  });
 });
 
 app.configure('development', function(){
   app.use(express.errorHandler());
+  mongoose.connect('mongodb://localhost/wedding');
 });
+
+app.configure('production', function() {
+  mongoose.connect('mongodb://nodejitsu:68a867653192c05aab1d8752a4e1ecef@dharma.mongohq.com:10097/nodejitsudb2830578889');
+});
+
+
+var Guest = mongoose.model('Guest', new Schema({
+    first_name  : { type: String, required: true, default: '' }
+  , last_name   : { type: String, required: true, default: '' }
+  , dinner      : { type: String, default: 'Chicken', validate: [requiredIfAttending, 'Please provide a dinner option.'] }
+  , adult       : Boolean
+}));
+
+function requiredIfAttending(val) {
+  if (this.parent.attending) {
+    return true;
+  } else {
+    return val && val.length > 0;
+  }
+}
+
+function notEmpty(val) {
+  return val && val.length > 0;
+}
+
+mongoose.model('RSVP', new Schema({
+    email       : { type: String, unique: true, required: true, default: '', validate: [notEmpty, 'Please provide your email address'] }
+  , guests      : [Guest.schema]
+  , date        : { type: Date, default: function() { return new Date(); } }
+  , attending   : Boolean
+}));
+
+
+app.get('/', routes.index);
+app.get('/rsvp', routes.rsvp.index);
+app.post('/rsvp', routes.rsvp.create);
+app.get('/rsvp/:id', routes.rsvp.show);
+app.post('/rsvp/:id', routes.rsvp.update);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,179 @@
+var mongoose = require('mongoose');
 
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.index = function(req, res) {
+  res.render('index', { title: 'Michael & Stacey - 10/5/2013' });
 };
+
+
+exports.rsvp = {
+  
+  index: function(req, res) {
+    var RSVP = mongoose.model('RSVP')
+      , query = req.param('query')
+      , rsvp = null;
+      
+    if (query) {
+      RSVP.findOne({ 'email': query }, 'email, guests, attending', function (err, rsvp) {
+        if (err) return handleError(err);
+        if (rsvp) {
+          res.redirect('rsvp/' + rsvp._id);
+        } else {
+          res.render('rsvp/index', { 
+            title: 'Michael &amp; Stacey - RSVP', 
+            rsvp: null,
+            query: query
+          });
+        }
+      });
+      return
+    } 
+    
+    res.render('rsvp/index', { 
+      title: 'Michael &amp; Stacey - RSVP', 
+      rsvp: null,
+      query: ''
+    });
+    
+  },
+  
+  new: function(req, res) {
+    
+  },
+  
+  update: function(req, res) {
+    
+    var RSVP = mongoose.model('RSVP')
+      , Guest = mongoose.model('Guest')
+      , rsvp = req.param('rsvp')
+      , guests = req.param('guests')
+      , id = req.param('id');
+    
+
+    if (req.param('save')) {
+      rsvp.guests = guests;
+      RSVP.update({_id: id, }, rsvp, function(err) {
+        if (err) {
+          console.log(err);
+          res.render('rsvp/show', {
+             title: 'Michael &amp; Stacey - Mange Reservation',
+             rsvp: rsvp
+          });
+          return;
+        }
+        res.redirect('/');
+      });
+      return;
+    }
+    
+    rsvp._id = id
+    rsvp = new RSVP(rsvp);
+    
+    // Add a new guest
+    if (req.param('add_guest')) guests.push({});
+    
+    for (var i=0; i<guests.length; i++) {
+      rsvp.guests.push(new Guest(guests[i]));
+    }
+    
+    
+    if (req.param('remove_guest')) {
+      var index = parseInt(req.param('remove_guest'), 10);
+      rsvp.guests[index].remove();
+    }
+    
+    res.render('rsvp/show', {
+       title: 'Michael &amp; Stacey - Mange Reservation',
+       rsvp: rsvp
+    });
+    
+  },
+  
+  create: function(req, res) {
+    var RSVP = mongoose.model('RSVP')
+      , Guest = mongoose.model('Guest')
+      , rsvp = req.param('rsvp') || {}
+      , guest = req.param('guest') || {};
+    
+    
+    rsvp = new RSVP(rsvp);
+    guest = new Guest(guest);
+    
+    if (req.param('save')) {
+      rsvp.guests.push(guest);
+      
+      return rsvp.save(function(err) {
+        if (err) {
+          
+          // Duplicate email
+          if (11000 === err.code || 11001 === err.code) {
+            res.locals.error = 'A RSVP has already been registed to that email address.'
+          } else if (err.errors) {
+            res.locals.error = "Please complete all fields."
+          } else {
+            res.locals.error = "An error occurred. Try again later or email mike.hemesath@gmail.com if the error continues."
+          }
+          
+          res.render('rsvp/new', { 
+            title: 'Michael &amp; Stacey - RSVP', 
+            rsvp: rsvp,
+            guest: guest
+          });
+        } else {
+          res.redirect('/rsvp/' + rsvp._id)
+        }
+      });
+    } 
+    
+    res.render('rsvp/new', { 
+      title: 'Michael &amp; Stacey - RSVP', 
+      rsvp: rsvp,
+      guest: guest,
+      error: false
+    });
+    
+  },
+  
+  show: function(req, res) {
+    var RSVP = mongoose.model('RSVP');
+    console.log(req.param('id'))
+    RSVP.findById(req.param('id'), function (err, rsvp) {
+      if (err) console.log(err);
+      
+      res.render('rsvp/show', {
+         title: 'Michael &amp; Stacey - Mange Reservation',
+         rsvp: rsvp
+      });
+    });
+    
+  }
+}
+/*
+
+exports.rsvp_new = function(req, res) {
+  var RSVP = mongoose.model('RSVP')
+    , Guest = mongoose.model('Guest');
+  
+  res.render('rsvp', { 
+    title: 'Michael &amp; Stacey - RSVP', 
+    rsvp: new RSVP({ attending: req.param('attending') === 'true' }),
+    guest: new Guest({})
+  });
+};
+
+
+exports.rsvp_create = function(req, res) {
+  var RSVP = mongoose.model('RSVP')
+    , Guest = mongoose.model('Guest')
+    , guestCount = req.param('guest_count');
+  
+  var rsvp = new RSVP(req.param('rsvp')),
+      guests = req.param('guests') || [];
+  
+  
+};
+
+*/
