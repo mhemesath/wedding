b50474e284c875e1637057aa28776799426d34a5
Michael Hemesath <michaelhemesath@Michaels-MacBook-Air.local>
2013-09-15 10:20:53 -0500

    Updated guest list to stop sending confirmation email

--- app.js
+++ app.js
@@ -7,11 +7,24 @@ var express = require('express')
   , routes = require('./routes')
   , http = require('http')
   , path = require('path')
-  , mongoose = require('mongoose');
+  , mongoose = require('mongoose')
+  , nodemailer = require('nodemailer')
+  , json = require('jsonify');
 
 var app = express()
   , Schema = mongoose.Schema
   , ObjectId = Schema.ObjectId;
+  
+  
+var smtpTransport = nodemailer.createTransport("SMTP",{
+    service: "Gmail",
+    auth: {
+        user: "mike.hemesath@gmail.com",
+        pass: "ovqqjhitipyiwyjh"
+    }
+});
+
+routes = routes(app);
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -35,19 +48,26 @@ app.configure(function(){
 app.configure('development', function(){
   app.use(express.errorHandler());
   mongoose.connect('mongodb://localhost/wedding');
+  app.locals({
+    siteURL: 'http://localhost:3000'
+  });
 });
 
 app.configure('production', function() {
   mongoose.connect('mongodb://nodejitsu:68a867653192c05aab1d8752a4e1ecef@dharma.mongohq.com:10097/nodejitsudb2830578889');
+  app.locals({
+    siteURL: 'http://www.mikestaceykc.com'
+  });
 });
 
 
 var Guest = mongoose.model('Guest', new Schema({
-    first_name  : { type: String, required: true, default: '' }
-  , last_name   : { type: String, required: true, default: '' }
+    first_name  : { type: String, default: '', validate: [nonEmpty, 'Guest first name is required.'] }
+  , last_name   : { type: String, default: '', validate: [nonEmpty, 'Guest last name is required.'] }
   , dinner      : { type: String, default: 'Chicken', validate: [requiredIfAttending, 'Please provide a dinner option.'] }
   , adult       : Boolean
 }));
+
 
 function requiredIfAttending(val) {
   if (this.parent.attending) {
@@ -57,12 +77,12 @@ function requiredIfAttending(val) {
   }
 }
 
-function notEmpty(val) {
-  return val && val.length > 0;
+function nonEmpty(val) {
+  return (val || '').replace(/ /g,'').length > 0;
 }
 
 mongoose.model('RSVP', new Schema({
-    email       : { type: String, unique: true, required: true, default: '', validate: [notEmpty, 'Please provide your email address'] }
+    email       : { type: String, unique: true, required: true, default: '', validate: [nonEmpty, 'Please provide your email address'] }
   , guests      : [Guest.schema]
   , date        : { type: Date, default: function() { return new Date(); } }
   , attending   : Boolean
@@ -72,9 +92,41 @@ mongoose.model('RSVP', new Schema({
 app.get('/', routes.index);
 app.get('/rsvp', routes.rsvp.index);
 app.post('/rsvp', routes.rsvp.create);
-app.get('/rsvp/:id', routes.rsvp.show);
-app.post('/rsvp/:id', routes.rsvp.update);
+app.get('/rsvp/:id', routes.rsvp.loadRSVP, routes.rsvp.show);
+app.post('/rsvp/:id',  routes.rsvp.loadRSVP, routes.rsvp.update);
 
+
+app.get('/guests', routes.guests);
+app.post('/guests', routes.guests);
+
+
+function emailBackup() {
+  mongoose.model('RSVP').find({}).lean().exec(function (err, rsvps) {
+    var mailOptions = {
+      from: "mikestaceykc.com <mike.hemesath@gmail.com>",
+      to: 'mike.hemesath@gmail.com',
+      subject: "Guest List Backup",
+      text: 'Attached is the updated guest list',
+      attachments: [
+        {   // utf-8 string as an attachment
+          fileName: "rsvps.json",
+          contents: json.stringify(rsvps)
+        }
+      ]
+    };
+      
+    smtpTransport.sendMail(mailOptions, function(error, response){
+      if(error){
+        console.log(error);
+    	  return;
+      }
+	  
+      console.log("Message sent: " + response.message);
+    });
+  });
+}
+
+setInterval(emailBackup, 1000 * 3600 * 24);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,68 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'),
+	nodemailer = require('nodemailer');
+	
+    
+var smtpTransport = nodemailer.createTransport("SMTP",{
+    service: "Gmail",
+    auth: {
+        user: "mike.hemesath@gmail.com",
+        pass: "ovqqjhitipyiwyjh"
+    }
+});
 
-/*
- * GET home page.
- */
+var routes = {
+}
 
-exports.index = function(req, res) {
+function emailConfirmation(rsvp) {
+  var firstName = rsvp.guests[0].first_name;
+  app.render('email', { rsvp: rsvp, firstName: firstName }, function(htmlErr, html){
+    app.render('email_text', { rsvp: rsvp, pretty: true }, function(textErr, text) {
+      var mailOptions = {
+        from: "mikestaceykc.com <mike.hemesath@gmail.com>",
+        to: rsvp.email,
+        subject: "RSVP Confirmation - Mike & Stacey's Wedding",
+        text: text,
+        html: html 
+      };
+        
+      smtpTransport.sendMail(mailOptions, function(error, response){
+        if(error){
+          console.log(error);
+      	  return;
+        }
+	
+        console.log("Message sent: " + response.message);
+      });
+    
+    });
+  });
+}
+
+
+routes.index = function(req, res) {
   res.render('index', { title: 'Michael & Stacey - 10/5/2013' });
 };
 
 
-exports.rsvp = {
+routes.guests = function(req, res) {
+  
+  if (req.param('password') !== 'thailand') {
+    res.render('login', {
+      title: 'Login'
+    });
+    return
+  }
+  
+  mongoose.model('RSVP').find({}, function (err, docs) {
+    res.render('guests', {
+      title: 'Guest List',
+      rsvps: docs
+    });
+  });
+};
+
+
+routes.rsvp = {
   
   index: function(req, res) {
     var RSVP = mongoose.model('RSVP')
@@ -49,40 +102,60 @@ exports.rsvp = {
     var RSVP = mongoose.model('RSVP')
       , Guest = mongoose.model('Guest')
       , rsvp = req.param('rsvp')
-      , guests = req.param('guests')
+      , guests = req.param('guests') || []
       , id = req.param('id');
-    
-
-    if (req.param('save')) {
-      rsvp.guests = guests;
-      RSVP.update({_id: id, }, rsvp, function(err) {
-        if (err) {
-          console.log(err);
-          res.render('rsvp/show', {
-             title: 'Michael &amp; Stacey - Mange Reservation',
-             rsvp: rsvp
-          });
-          return;
-        }
-        res.redirect('/');
-      });
-      return;
-    }
     
     rsvp._id = id
     rsvp = new RSVP(rsvp);
-    
-    // Add a new guest
-    if (req.param('add_guest')) guests.push({});
-    
     for (var i=0; i<guests.length; i++) {
       rsvp.guests.push(new Guest(guests[i]));
     }
     
+    res.locals.error = false;
     
+    // Save Guest List
+    if (req.param('save')) {
+      
+      req.rsvp.attending = rsvp.attending;
+      req.rsvp.email = rsvp.email;
+      req.rsvp.guests = [];
+      
+      for (var i=0; i< rsvp.guests.length; i++) {
+        req.rsvp.guests.push(rsvp.guests[i]);
+      }
+      
+      req.rsvp.save(function(err) {
+        if (err) {
+          if (err.errors) {
+            res.locals.error = "Please ensure all guest information is filled out."
+          } else {
+            res.locals.error = "An error occurred. Try again later or email mike.hemesath@gmail.com if the error continues."
+            console.log(err);
+          }
+          
+          res.render('rsvp/show', {
+             title: 'Michael &amp; Stacey - Mange Reservation',
+             rsvp: req.rsvp
+          });
+          return;
+        }
+		
+		    // Succesfully Update
+		    //emailConfirmation(rsvp);
+        res.render('rsvp/confirmation', {
+			    title: 'Michael &amp; Stacey - RSVP Confirmation',
+			    rsvp: req.rsvp
+        });
+      });
+      return;
+    }
+    
+    // Add a new guest
+    if (req.param('add_guest')) rsvp.guests.push(new Guest({}));
+    
+    // Remove Guest
     if (req.param('remove_guest')) {
-      var index = parseInt(req.param('remove_guest'), 10);
-      rsvp.guests[index].remove();
+      rsvp.guests.id(req.param('remove_guest')).remove();
     }
     
     res.render('rsvp/show', {
@@ -138,42 +211,26 @@ exports.rsvp = {
   },
   
   show: function(req, res) {
+    res.render('rsvp/show', {
+       title: 'Michael &amp; Stacey - Mange Reservation',
+       rsvp: req.rsvp,
+       error: false
+    });
+  },
+  
+  loadRSVP: function(req, res, next) {
     var RSVP = mongoose.model('RSVP');
-    console.log(req.param('id'))
     RSVP.findById(req.param('id'), function (err, rsvp) {
       if (err) console.log(err);
-      
-      res.render('rsvp/show', {
-         title: 'Michael &amp; Stacey - Mange Reservation',
-         rsvp: rsvp
-      });
+      req.rsvp = rsvp;
+      next();
     });
-    
-  }
+  },
 }
-/*
-
-exports.rsvp_new = function(req, res) {
-  var RSVP = mongoose.model('RSVP')
-    , Guest = mongoose.model('Guest');
-  
-  res.render('rsvp', { 
-    title: 'Michael &amp; Stacey - RSVP', 
-    rsvp: new RSVP({ attending: req.param('attending') === 'true' }),
-    guest: new Guest({})
-  });
-};
 
 
-exports.rsvp_create = function(req, res) {
-  var RSVP = mongoose.model('RSVP')
-    , Guest = mongoose.model('Guest')
-    , guestCount = req.param('guest_count');
-  
-  var rsvp = new RSVP(req.param('rsvp')),
-      guests = req.param('guests') || [];
-  
-  
-};
+module.exports = function(express) {
+  app = express;
+  return routes;
+}
 
-*/
